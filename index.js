42a0d350d8c673335ca52682d0c16b468cd68de3
Thad Paget <fubekafilms@gmail.com>
2017-03-11 13:48:48 -0700

    chat api functionallity added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+var app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [{message: 'What\'s up!!', time: new Date()}];
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({messages: messages})
+});
+
+app.post('/messages', function(req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({messages: messages});
+  console.log(req.body);
+});
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000')
+});
